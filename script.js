88d1eb34ba93b8fc2647736686d5a25c01c9a1b1
xtang999 <xiaoli05tang@gmail.com>
2023-10-26 14:06:31 +1100

    initialisation

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+function operate(x, y, operator) {
+    if (operator === '+') {
+        return add(x, y);
+    } else if (operator === '-') {
+        return subtract(x, y);
+    } else if (operator === '*') {
+        return multiply(x, y);
+    } else (operator === '/') {
+        return divide(x, y);
+    }
+}
